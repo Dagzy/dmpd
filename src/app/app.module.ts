1180b1b009120585360aa83645e430d3bf25dfa9
Michelle Hoelzer <michellehoelzer@Michelles-MacBook-Pro.local>
2017-08-01 16:56:11 -0400

    idek

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule} from '@angular/router'
 
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { LandingComponent } from './landing/landing.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [LandingComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
