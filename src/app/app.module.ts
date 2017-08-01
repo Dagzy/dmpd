8666522ec1c1eec464a24ade21bae181c5e53b37
Bryce  Greene <bgreene@elevenfiftyconsulting.com>
2017-08-01 16:17:21 -0400

    added stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule} from '@angular/router'
 
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [LandingComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
