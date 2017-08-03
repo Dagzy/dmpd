66d495fd4951a682caa6fff5117fc627069381c0
Bryce  Greene <bgreene@elevenfiftyconsulting.com>
2017-08-03 09:40:38 -0400

    landing page updated, alsso aded firebase auth and whatnot

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes} from '@angular/router'
 
@@ -6,6 +7,23 @@ import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { LandingComponent } from './landing/landing.component';
 import { NavComponent } from './nav/nav.component';
+
+import { AngularFireModule } from 'angularfire2';
+
+// New imports to update based on AngularFire2 version 4
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+  var config = {
+    apiKey: "AIzaSyBBWJJG70QSKgGlWzz5D3lXgZitVNDp-XQ",
+    authDomain: "split-500b7.firebaseapp.com",
+    databaseURL: "https://split-500b7.firebaseio.com",
+    projectId: "split-500b7",
+    storageBucket: "",
+    messagingSenderId: "524073548350"
+  };
+
+
 
 const appRoutes: Routes = [
   {
@@ -18,10 +36,14 @@ const appRoutes: Routes = [
     AppComponent,
     HomepageComponent,
     LandingComponent,
-    NavComponent
+    NavComponent,
+   
   ],
   imports: [
+     AngularFireModule.initializeApp(config),
     BrowserModule,
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
     RouterModule.forRoot(
     	appRoutes,
     	{enableTracing: true} //debugging
