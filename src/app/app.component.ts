139980d6a02dead78186d27d0d8faa8380f045b8
mhoelzer <hoelzermm@gmail.com>
2017-10-04 09:51:47 -0400

    jd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,37 +5,37 @@ import * as firebase from 'firebase/app';
 import { AngularFireAuth, AngularFireAuthModule } from 'angularfire2/auth';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   user: Observable<firebase.User>;
 
-    constructor( public db: AngularFireDatabase, public afAuth: AngularFireAuth, public af: AngularFireDatabase) {
-        // this.user = this.afAuth.auth.currentUser;
+	constructor( public db: AngularFireDatabase, public afAuth: AngularFireAuth, public af: AngularFireDatabase) {
+		// this.user = this.afAuth.auth.currentUser;
 
-    }
-    navShouldBe(){ // a function that determines whether or not the nav bar should appear.
-        let state: boolean;
-        const url = window.location.href;
-        let indexOfSlash: number;
-        for (let i = 0; i < url.length; i++){
-            if (url[i] === '/'){
-                indexOfSlash = i;
+	}
+	navShouldBe(){ // a function that determines whether or not the nav bar should appear.
+		let state: boolean;
+		const url = window.location.href;
+		let indexOfSlash: number;
+		for (let i = 0; i < url.length; i++){
+			if (url[i] === '/'){
+				indexOfSlash = i;
 
-            }
-        }
-        const urlEnd = url.substring(indexOfSlash, url.length);
-        if (urlEnd === '/' || urlEnd === '/landing'){
-            state = true;
-            // make ^false if dont want the menu there
-        } else {
-            state = true;
-        }
-        return state;
-    }
-    title = 'app';
+			}
+		}
+		const urlEnd = url.substring(indexOfSlash, url.length);
+		if (urlEnd === '/' || urlEnd === '/landing'){
+			state = true;
+			// make ^false if dont want the menu there
+		} else {
+			state = true;
+		}
+		return state;
+	}
+	title = 'app';
 
 // loginFacebook() {
 //     this.afAuth.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider())
@@ -44,7 +44,3 @@ export class AppComponent {
 //     })
 //   }
 }
-
-
-
-
