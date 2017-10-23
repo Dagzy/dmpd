6523b9b357e7b3589c0084ce75b7cc3a46dbf345
mhoelzer <hoelzermm@gmail.com>
2017-10-23 10:39:40 -0400

    gixed landing and nav

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -75,6 +75,6 @@ export class LandingComponent implements OnInit {
     this.afAuth.signup(user)// runs the signup function in authService.ts
   }
   fbo(){
-     this.suLi = !this.fbo
+     this.suLi = !this.suLi
   }
 }
